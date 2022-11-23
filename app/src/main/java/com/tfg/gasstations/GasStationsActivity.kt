c4e46dd89bde57f5c2f8ed3fd07639821e0a1158
@SamuelBosonit <samuel.fuentes@bosonit.com>
2022-11-23 20:26:16 +0100

    ADD signup, recover password, app activities -> SignUpActivity, ForgotPasswordActivity, GasStationsActivity

---
+++ app/src/main/java/com/tfg/gasstations/GasStationsActivity.kt
@@ -0,0 +1,11 @@
+package com.tfg.gasstations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GasStationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gas_stations)
+    }
+}
