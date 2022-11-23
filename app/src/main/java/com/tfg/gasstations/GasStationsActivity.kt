3b91be666af4d33664bf6ae373fae22fea60a120
@SamuelBosonit <samuel.fuentes@bosonit.com>
2022-11-23 22:20:06 +0100

    ADD signOut, REMOVE back button -> GasStationsActivity

--- app/src/main/java/com/tfg/gasstations/GasStationsActivity.kt
+++ app/src/main/java/com/tfg/gasstations/GasStationsActivity.kt
@@ -1,11 +1,44 @@
 package com.tfg.gasstations
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class GasStationsActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gas_stations)
+
+        firebaseAuth = Firebase.auth
+    }
+    //Mostrar menú
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu,menu)
+        return true
+    }
+    //Cerrar sesión
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_signOut -> {
+                signOut()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun signOut(){
+        firebaseAuth.signOut()
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+    }
+    //Anular botón back
+    override fun onBackPressed() {
+        return
     }
 }
