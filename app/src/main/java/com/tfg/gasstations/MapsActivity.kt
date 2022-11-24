b04ed21ddba656002d3abb21237ea343dabd2afa
@SamuelBosonit <samuel.fuentes@bosonit.com>
2022-11-24 19:39:01 +0100

    ADDED permissions and resqued permission of localization -> MapsActivity

--- app/src/main/java/com/tfg/gasstations/MapsActivity.kt
+++ app/src/main/java/com/tfg/gasstations/MapsActivity.kt
@@ -1,7 +1,14 @@
 package com.tfg.gasstations
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -9,19 +16,70 @@ import com.google.android.gms.maps.SupportMapFragment
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         createMapFragment()
     }
-
-    override fun onMapReady(googleMap: GoogleMap) {
+    //Arrancar GoogleMaps
+    override fun onMapReady(googleMap: GoogleMap){
         map = googleMap
+        enableMyLocation()
     }
-
-    private fun createMapFragment() {
+    @SuppressLint("MissingSuperCall", "MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray){
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                map.isMyLocationEnabled = true
+            }
+            else{
+                Toast.makeText(this, "Active permisos de localización", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+        }
+    }
+    //Al maximizar la aplicación, vuelve a comprobar los permisos de ubicación
+    @SuppressLint("MissingPermission")
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (!::map.isInitialized) return
+        if(!isPermissionsFineGranted()){
+            map.isMyLocationEnabled = false
+            Toast.makeText(this, "Active permisos de localización", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun createMapFragment(){
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
+    //Comprueba si están los permisos de ubicación
+    private fun isPermissionsFineGranted() = ContextCompat.checkSelfPermission(
+        this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    //Activa la localización si están los permisos de ubicación concedidos
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation(){
+        if (!::map.isInitialized) return
+        if (isPermissionsFineGranted()){
+            map.isMyLocationEnabled = true
+        }
+        else{
+            requestLocationPermission()
+        }
+    }
+    //Solicita permisos de ubicación
+    private fun requestLocationPermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(this, "Active permisos de localización", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
+        }
+    }
+    //TODO función para iniciar en tu localización actual
+    private fun myStartingLocalization(){}
 }
