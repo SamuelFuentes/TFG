735895874b358f8d3b0419c4ae05d43eb2700167
@SamuelBosonit <samuel.fuentes@bosonit.com>
2022-11-29 03:27:22 +0100

    FIXED spinner for cities, filter cities, add lowerPricesMarker -> MapsActivity

--- app/src/main/java/com/tfg/gasstations/MapsActivity.kt
+++ app/src/main/java/com/tfg/gasstations/MapsActivity.kt
@@ -6,13 +6,13 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.icu.number.IntegerWidth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
 import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -41,6 +41,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var markerBp : BitmapDescriptor
     private lateinit var markerGalp : BitmapDescriptor
     private lateinit var markerShell : BitmapDescriptor
+    private lateinit var markerMinGas95 : BitmapDescriptor
+    private lateinit var markerMinGas98 : BitmapDescriptor
+    private lateinit var markerMinGasol : BitmapDescriptor
+    private lateinit var spinnerCities : Spinner
+    private lateinit var selectedCity : String
+    private var idSelectedCity : String = "00"
 
     private var start : String = ""
     private var end   : String = ""
@@ -52,7 +58,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         createMapFragment()
-        markerGasByCity()
+        listCities()
         //markerCepsa
         val MarkerViewCepsa = (getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.custom_markers, null)
         val cardViewCepsa : CardView = MarkerViewCepsa.findViewById(R.id.cardViewCepsa)
@@ -78,6 +84,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val cardViewShell : CardView = markerViewShell.findViewById(R.id.cardViewShell)
         val bitmapShell = Bitmap.createScaledBitmap(viewToBitmap(cardViewShell), cardViewShell.width, cardViewShell.height, false)
         markerShell = BitmapDescriptorFactory.fromBitmap(bitmapShell)
+        //markerMinGas95
+        val markerViewMinGas95 = (getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.custom_markers, null)
+        val cardViewMinGas95 : CardView = markerViewMinGas95.findViewById(R.id.cardViewMinGas95)
+        val bitmapMinGas95 = Bitmap.createScaledBitmap(viewToBitmap(cardViewMinGas95), cardViewMinGas95.width, cardViewMinGas95.height, false)
+        markerMinGas95 = BitmapDescriptorFactory.fromBitmap(bitmapMinGas95)
+        //markerMinGas98
+        val markerViewMinGas98 = (getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.custom_markers, null)
+        val cardViewMinGas98 : CardView = markerViewMinGas98.findViewById(R.id.cardViewMinGas98)
+        val bitmapMinGas98 = Bitmap.createScaledBitmap(viewToBitmap(cardViewMinGas98), cardViewMinGas98.width, cardViewMinGas98.height, false)
+        markerMinGas98= BitmapDescriptorFactory.fromBitmap(bitmapMinGas98)
+        //markerMinGasol
+        val markerViewMinGasol = (getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.custom_markers, null)
+        val cardViewMinGasol : CardView = markerViewMinGasol.findViewById(R.id.cardViewMinGasol)
+        val bitmapMinGasol = Bitmap.createScaledBitmap(viewToBitmap(cardViewMinGasol), cardViewMinGasol.width, cardViewMinGasol.height, false)
+        markerMinGasol = BitmapDescriptorFactory.fromBitmap(bitmapMinGasol)
 
         val buttonClear : Button = findViewById(R.id.buttonClear)
 
@@ -112,6 +133,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
         buttonClear.setOnClickListener{
             map.clear()
+        }
+        //Spinner para filtro por ciudades
+        var citiesArrayList: List<String> = listCities()
+        spinnerCities = findViewById(R.id.spinnerCities)
+        spinnerCities.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, citiesArrayList)
+        spinnerCities.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                selectedCity = citiesArrayList[p2]
+                searchIdCity(selectedCity)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                "Error"
+            }
         }
     }
     //Arrancar GoogleMaps
@@ -218,34 +253,62 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val gasArrayList = ArrayList<String>()
         CoroutineScope(Dispatchers.IO).launch {
             val call = getGasStations().create(ApiServiceGasByCity::class.java)
-                .getGasStationsByCity("41")
-            //35
+                .getGasStationsByCity(idSelectedCity)
+            //35 41 38
             if(call.isSuccessful){
-                Log.i("DEPURANDO", "SUCCES")
-                for (i in call.body()?.gasList!!){
-                    gasArrayList.add(i.label)
-                }
-                Log.i("DEPURANDO", gasArrayList.toString())
                 if(::map.isInitialized){
                     runOnUiThread {
+                        var minGas95 = 99.99
+                        var minGas98 = 99.99
+                        var minGasol = 99.99
                         for (i in call.body()!!.gasList){
-                            //var labelIcon = selectIcon(gasArrayList)
                             var markerIcon = selectMarkerIcon(i.label)
-                            val lat = i.lati.replace(",",".").toDouble()
-                            val lng = i.long.replace(",",".").toDouble()
-                            val position = LatLng(lat, lng)
-                            var gas95 = ""
-                            var gas98 = ""
-                            var gasol = ""
-                            if(i.gas95.isNotEmpty()){ gas95 = "Gas 95: "+ i.gas95 +"€, " }
-                            if(i.gas98.isNotEmpty()){ gas98 = "Gas 98: "+ i.gas98 +"€, " }
-                            if(i.gasoleo.isNotEmpty()){ gasol = "Gasoleo: "+ i.gasoleo +"€, " }
+                            var position = convertApiPosToLatLng(i.lati, i.long)
+                            var gasTypeForSnippet = isGasType(i.gas95, i.gas98, i.gasol)
+                            if(i.gas95.isNotEmpty()){ minGas95 = minPrice(minGas95, i.gas95) }
+                            if(i.gas98.isNotEmpty()){ minGas98 = minPrice(minGas98, i.gas98) }
+                            if(i.gasol.isNotEmpty()){ minGasol = minPrice(minGasol, i.gasol) }
                             map.addMarker(
                                 MarkerOptions().position(position).title(
                                     "${i.label}, ${i.address} Horario: ${i.schedule}")
-                                    .snippet("$gas95$gas98$gasol").icon(markerIcon)
-                                    //.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                                    .snippet(gasTypeForSnippet[0]+gasTypeForSnippet[1]+gasTypeForSnippet[2]).icon(markerIcon)
                             )
+                        }
+                        for(i in call.body()!!.gasList){
+                            if(i.gas95.isNotEmpty()){
+                                if(i.gas95.replace(",",".").toDouble() == minGas95){
+                                    var position = convertApiPosToLatLng(i.lati, i.long)
+                                    var gasTypeForSnippet = isGasType(i.gas95, i.gas98, i.gasol)
+                                    map.addMarker(
+                                        MarkerOptions().position(position).title(
+                                            "${i.label}, ${i.address} Horario: ${i.schedule}")
+                                            .snippet(gasTypeForSnippet[0]+gasTypeForSnippet[1]+gasTypeForSnippet[2]).icon(markerMinGas95))
+                                }
+                            }
+                        }
+                        for(i in call.body()!!.gasList){
+                            if(i.gas98.isNotEmpty()){
+                                if(i.gas98.replace(",",".").toDouble() == minGas95){
+                                    var position = convertApiPosToLatLng(i.lati, i.long)
+                                    var gasTypeForSnippet = isGasType(i.gas95, i.gas98, i.gasol)
+                                    map.addMarker(
+                                        MarkerOptions().position(position).title(
+                                            "${i.label}, ${i.address} Horario: ${i.schedule}")
+                                            .snippet(gasTypeForSnippet[0]+gasTypeForSnippet[1]+gasTypeForSnippet[2]).icon(markerMinGas98))
+                                }
+                            }
+                        }
+                        for(i in call.body()!!.gasList){
+                            if(i.gasol.isNotEmpty()){
+                                if(i.gasol.replace(",",".").toDouble() == minGas95){
+                                    var position = convertApiPosToLatLng(i.lati, i.long)
+                                    var gasTypeForSnippet = isGasType(i.gas95, i.gas98, i.gasol)
+                                    map.addMarker(
+                                        MarkerOptions().position(position).title(
+                                            "${i.label}, ${i.address} Horario: ${i.schedule}")
+                                            .snippet(gasTypeForSnippet[0]+gasTypeForSnippet[1]+gasTypeForSnippet[2]).icon(markerMinGasol))
+                                }
+                            }
                         }
                     }
                 }
@@ -273,27 +336,77 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val bitmapDefault = Bitmap.createScaledBitmap(viewToBitmap(cardViewDefault), cardViewDefault.width, cardViewDefault.height, false)
         markerDefault = BitmapDescriptorFactory.fromBitmap(bitmapDefault)
         var markerRes = BitmapDescriptorFactory.fromBitmap(bitmapDefault)
-        if(label == "CEPSA"){ markerRes = markerCepsa }
+        if(label == "CEPSA"){
+
+            markerRes = markerCepsa
+        }
         if(label == "REPSOL"){ markerRes = markerRepsol }
         if(label == "BP"){ markerRes = markerBp }
         if(label == "GALP"){ markerRes = markerGalp }
         if(label == "SHELL"){ markerRes = markerShell }
         return markerRes
     }
-    /*
-    private fun myLocation(){
-        CoroutineScope(Dispatchers.IO).launch{
-            if(::map.isInitialized){
-                runOnUiThread {
-                    val myPosition = LatLng(map.myLocation.latitude, map.myLocation.longitude)
-                    Log.i("DEPURANDO","myposition ${myPosition}")
-                    map.animateCamera(
-                        CameraUpdateFactory.newLatLngZoom(myPosition, 16f),
-                        4000,
-                        null
-                    )
+    //Convierte las coordenadas de latitud y longitud de la api en LatLng utilizables por googleMaps
+    private fun convertApiPosToLatLng(lat : String, lng : String): LatLng{
+        return LatLng(lat.replace(",",".").toDouble(),
+                                lng.replace(",",".").toDouble())
+    }
+    //Mostrará unicamente los productos que tengan el precio en la API
+    private fun isGasType(apiGas95 : String, apigas98 : String, apigasol : String):ArrayList<String>{
+        var gas95 = ""
+        var gas98 = ""
+        var gasol = ""
+        val gasTypeArrayList = ArrayList<String>()
+        if(apiGas95.isNotEmpty()){ gas95 = "Gas 95: "+ apiGas95 +"€, " }
+        if(apigas98.isNotEmpty()){ gas98 = "Gas 98: "+ apigas98 +"€, " }
+        if(apigasol.isNotEmpty()){ gasol = "Gasoleo: "+ apigasol +"€, " }
+        gasTypeArrayList.add(gas95)
+        gasTypeArrayList.add(gas98)
+        gasTypeArrayList.add(gasol)
+        return gasTypeArrayList
+    }
+    //Calcula el precio mínimo de cada combustible
+    private fun minPrice(minPrice : Double, newPrice : String) : Double{
+        var res = newPrice.replace(",",".").toDouble()
+        if(res > minPrice){ res = minPrice }
+        return res
+    }
+
+    //Llamada a la API y conversión del JSON
+    private fun getCities(): Retrofit {
+        return Retrofit.Builder().baseUrl("https://sedeaplicaciones.minetur.gob.es/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+    //Crear lista de ciudades para mostrarlas en el Spinner
+    private fun listCities(): List<String> {
+        val citiesArrayList : MutableList<String> = mutableListOf("")
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getCities().create(ApiServiceCities::class.java).getCities()
+            if (call.isSuccessful) {
+                for (i in call.body()!!) {
+                    citiesArrayList.add(i.provincia)
                 }
+                Log.i("DEPURANDO", citiesArrayList.toString())
+            } else {
+                Log.i("DEPURANDO", "NOT SUCCES")
             }
         }
-    }*/
+        return citiesArrayList
+    }
+    //Buscar la id de la ciudad elegida en el Spinner
+    private fun searchIdCity(city : String){
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getCities().create(ApiServiceCities::class.java).getCities()
+            if (call.isSuccessful) {
+                Log.i("DEPURANDO", "SUCCESsssss")
+                for (i in call.body()!!) {
+                    if(city == i.provincia){
+                        idSelectedCity = i.idProvincia
+                        Log.i("DEPURANDO", "id"+idSelectedCity)
+                    }
+                }
+                markerGasByCity()
+            }
+        }
+    }
 }
--- app/src/main/java/com/tfg/gasstations/Reponses.kt
+++ app/src/main/java/com/tfg/gasstations/Reponses.kt
@@ -11,7 +11,7 @@ data class GasList(@SerializedName("Dirección") var address : String,
                    @SerializedName("Horario") var schedule : String,
                    @SerializedName("Latitud") var lati : String,
                    @SerializedName("Longitud (WGS84)") var long : String,
-                   @SerializedName("Precio Gasoleo A") var gasoleo : String,
+                   @SerializedName("Precio Gasoleo A") var gasol : String,
                    @SerializedName("Precio Gasolina 95 E5") var gas95 : String,
                    @SerializedName("Precio Gasolina 98 E5") var gas98 : String,
                    @SerializedName("Rótulo") var label : String)
